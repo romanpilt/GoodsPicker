a698db73df34de3c869c0a0856a29c6f94d65aa5
Roman Piltenko <rpiltenko@bitlish.com>
2019-12-23 11:07:36 +0300

    Initial Commit

---
+++ GoodsPicker/ServiceKeys.swift
@@ -0,0 +1,16 @@
+//
+//  ServiceKeys.swift
+//  GoodsPicker
+//
+//  Created by Roman on 23/12/2019.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import Foundation
+
+struct ServiceKeys {
+    struct Unsplash {
+        static let accessKey = "insert_key_here"
+        static let secretKey = "insert_key_here"
+    }
+}
---
+++ GoodsPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodsPicker
+//
+//  Created by Bitlish on 23/12/2019.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
